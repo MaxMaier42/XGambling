894a2fb89b2513a8999ac6c435a1330c28a5e47f
MaxMaier42 <48088213+MaxMaier42@users.noreply.github.com>
2024-06-28 19:03:31 +0100

    update

---
+++ R/XGambling.R
@@ -0,0 +1,6 @@
+#' @title XGambling
+#' @description Functions for the Extinction Gambling Task
+#' @details Click on 'index' below to see all the functions.
+#' @name package-package
+#' @docType _PACKAGE
+NULL
